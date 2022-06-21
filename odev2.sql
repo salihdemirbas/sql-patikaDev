62f7b10b4ee3dc84a5f5c51b1679e1378259ae94
Salih Demirbaş <salihdemirbs@gmail.com>
2022-06-21 20:59:20 +0300

    Create odev2.sql

---
+++ odev2.sql
@@ -0,0 +1,10 @@
+--film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız.
+
+select * from film WHERE replacement_cost between 12.99 and 16.99
+
+--actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız.
+
+select * from actor where first_name IN ('Penolope','Nick','Ed');
+
+--film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. 
+select * from film where (rental_rate IN (0.99,2.99,4.99)) and (replacement_cost IN (12.99,15,99,28,99))
